1fdb2da63865f60baf6dd4082d93e3bfc47e4831
Moges <Moges.ash@gmail.com>
2020-09-28 14:37:32 +0200

    Die controleert of een nieuw doel € 0 heeft opgebracht.

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -18,5 +18,6 @@ class GoedeDoelTest {
 	void getNaam() {
 		assertThat(doel.getNaam()).isEqualTo(NAAM);
 	}
-
+	@Test
+	void eenNieuwDoelHeeftNogGeenOpbrengst() { assertThat(doel.getOpbrengst()).isZero(); }
 }
